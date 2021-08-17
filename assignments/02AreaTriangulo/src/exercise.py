4cad420a6cfdbd3cdf3d37bf442baf8368967fef
A01740689 <A01740689@itesm.mx>
2021-08-17 13:59:46 +0000

    tarea terminada

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -3,9 +3,9 @@ def main():
     n1 = float(input('Dame la base: '))
     n2 = float(input('Dame la altura: '))
     
-    area = (n1 * n2)/2 
+    area = (n1 * n2)/2  
     
-    print('El area es: '+str(area))
+    print('El área es: '+str(area))
 
 if __name__ == '__main__':
     main()
